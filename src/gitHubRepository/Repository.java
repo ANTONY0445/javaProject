e0f6e1e31c2744d8df7ffcb6e2d91003674633c9
ANTONY0445 <victorantony0110@gmail.com>
2025-03-11 22:43:04 +0530

    javaQA

--- src/gitHubRepository/Repository.java
+++ src/gitHubRepository/Repository.java
@@ -5,7 +5,8 @@ public class Repository {
 	private void antony() {
 
 		System.out.println("my name is antony");
-		
+		System.out.println("today my work is pull the project from the repository");
+		System.out.println("pull the project from the github repo done");
 	}
 
 }
